35cc296df83992dcee9e2049f252da0976ffa35f
Rakesh Rudra <Rakesh.Rudra@jda.com>
2018-07-29 15:38:26 +0530

    Update monglab db user and passwd

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,14 @@
 var env = process.env.NODE_ENV || 'development' ;
 console.log('Env ......', env);
-
+var user ='rakesh';
+var passwrd ='rakesh123'; 
 if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.PORT = 3000; 
+   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else if (env === 'production') {
-    process.env.MONGODB_URI = 'mongodb://Rakesh R:Rakesh123$@ds257981.mlab.com:57981/rakesh_todotask'
+    //Mongolab database user: rakesh, passwd: rakesh123
+    process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
 }
