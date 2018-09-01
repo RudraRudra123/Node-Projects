acc12354e1fa246bf69f09c622409e5019b78f03
Rakesh Rudra <Rakesh.Rudra@jda.com>
2018-09-02 01:19:03 +0530

    jwt_Secret id setup

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,8 @@
 let env = process.env.NODE_ENV||'development' ;
 console.log('Environment is...', env);
 
+process.env.JWT_SECRET = 'suhas123';
+
 if(env === 'development') {
     process.env.PORT = 3000; 
    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -8,7 +10,6 @@ if(env === 'development') {
 } else if(env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-    process.env.JWT_SECRET = 'suhas123'
 } else if (env === 'production') {
     //Mongolab database user: rakesh, passwd: rakesh123
     process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
