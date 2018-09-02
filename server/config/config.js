2b66bde0c50cacb9d66c06e2e1546b40ef589874
Rakesh Rudra <Rakesh.Rudra@jda.com>
2018-09-02 12:48:39 +0530

    add config file to gitignore. set env variables on heroku

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,16 @@ process.env.JWT_SECRET = 'suhas123';
 if(env === 'development') {
     process.env.PORT = 3000; 
    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-   // process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
+   
 } else if(env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else if (env === 'production') {
+
+    //Environment variables for production env are set in heroku env variables list
+    // heroku config: set 
+    //heroku config: set process.env.JWT_SECRET = 'ds345#$@!mksdf'
+    //heroku config: set process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask'
     //Mongolab database user: rakesh, passwd: rakesh123
-    process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
+    //process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
 }
