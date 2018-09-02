b4719e75dbde180ed70b4e0528aaac10e0486f52
Rakesh Rudra <Rakesh.Rudra@jda.com>
2018-09-02 12:54:33 +0530

    config file changes

--- server/config/config.js
+++ server/config/config.js
@@ -13,9 +13,4 @@ if(env === 'development') {
 } else if (env === 'production') {
 
     //Environment variables for production env are set in heroku env variables list
-    // heroku config: set 
-    //heroku config: set process.env.JWT_SECRET = 'ds345#$@!mksdf'
-    //heroku config: set process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask'
-    //Mongolab database user: rakesh, passwd: rakesh123
-    //process.env.MONGODB_URI = 'mongodb://rakesh:rakesh123@ds257981.mlab.com:57981/rakesh_todotask';
-}
+  }
